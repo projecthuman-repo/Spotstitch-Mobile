c0358d934e91c31c208e4f8dcff0b7f5bf14f8f4
Onurcan Sever <onsever@icloud.com>
2023-03-13 11:07:29 -0400

    created social media icons component and list

---
+++ Spotstitch/src/components/Icon/socialIconList.js
@@ -0,0 +1,30 @@
+export const socialIconList = [
+  {
+    id: 1,
+    name: 'facebook',
+    onPress: () => {
+      console.log('facebook')
+    },
+  },
+  {
+    id: 2,
+    name: 'twitter',
+    onPress: () => {
+      console.log('twitter')
+    },
+  },
+  {
+    id: 3,
+    name: 'instagram',
+    onPress: () => {
+      console.log('instagram')
+    },
+  },
+  {
+    id: 4,
+    name: 'google',
+    onPress: () => {
+      console.log('google')
+    },
+  },
+]
