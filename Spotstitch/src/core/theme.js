1eac810302ba1f22a2505b16a35be516c720e786
Jovolynn Gragasin <jovolynngragasin@gmail.com>
2023-04-24 20:41:46 -0700

    editing theme

--- Spotstitch/src/core/theme.js
+++ Spotstitch/src/core/theme.js
@@ -5,8 +5,8 @@ export const theme = {
   colors: {
     // ...DefaultTheme.colors,
     text: '#000000',
-    primary: '#560CCE',
-    secondary: '#414757',
+    primary: '#8CBDEB',
+    secondary: '#A5DC7A',
     error: '#f13a59',
     link: '#007BED',
     gray: '#C7C9CB',
